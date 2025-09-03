0bf089ef34dd3f3fbb26aaddaf80c5b30f982c39
gasakiri <morisakirita@tutamail.com>
2025-09-03 15:00:12 -0300

    Lesson 01

---
+++ 01_lesson/math.js
@@ -0,0 +1,7 @@
+export function add(a, b) { return a + b; }
+export function subtract(a, b) { return a - b; }
+export function multiply(a, b) { return a * b; }
+export function divide(a, b) { return a / b; }
+
+/* module.exports = { add, subtract, multiply, divide }; old*/
+/* export default { add, subtract, multiply, divide }; */
---
+++ 01_lesson/server.js
@@ -0,0 +1,9 @@
+/* import math from "./math.js";
+const { add, subtract, multiply, divide } = math; */
+
+import { add, subtract, multiply, divide } from "./math.js";
+
+console.log(add(2, 2));
+console.log(subtract(2, 2));
+console.log(multiply(2, 2));
+console.log(divide(2, 2));
